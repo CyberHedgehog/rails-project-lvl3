18efc01f0a5d2585cf1ba419388d897d70a3add2
dmitriy <d.dobrenkiy@hotmail.com>
2021-12-23 21:55:23 +0300

    feat: bulletins resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :bulletins
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
-  root 'home#index'
+  root 'bulletins#index'
 end
