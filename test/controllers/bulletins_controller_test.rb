f2f7c644e0277073619c25bb307c06060f51468f
dmitriy <d.dobrenkiy@hotmail.com>
2021-12-27 22:31:31 +0300

    bulletins test

--- test/controllers/bulletins_controller_test.rb
+++ test/controllers/bulletins_controller_test.rb
@@ -3,7 +3,62 @@
 require 'test_helper'
 
 class BulletinsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+  setup do
+    @bulletin = bulletins(:one)
+    @user = users(:one)
+    @category = categories(:one)
+    @title = Faker::Lorem.sentence
+    @description = Faker::Lorem.paragraph
+    sign_in @user
+  end
+
+  test 'should get index' do
+    get bulletins_path
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get new_bulletin_path
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get edit_bulletin_path(@bulletin)
+    assert_response :success
+  end
+
+  test 'should show' do
+    sign_out :user
+    get bulletin_path(@bulletin)
+    assert_response :success
+  end
+
+  test 'should create' do
+    post bulletins_path, params: { bulletin: {
+      title: @title,
+      description: @description,
+      category_id: @category.id
+    } }
+    new_bulletin = Bulletin.find_by(title: @title)
+    assert new_bulletin
+    assert_redirected_to bulletin_path(new_bulletin)
+  end
+
+  test 'should update' do
+    put bulletin_path(@bulletin), params: { bulletin: {
+      title: @title,
+      description: @description,
+      category_id: @bulletin.category.id
+    } }
+    bulletin = Bulletin.find(@bulletin.id)
+    assert_redirected_to bulletin_path(@bulletin)
+    assert bulletin.title, @title
+    assert bulletin.description, @description
+  end
+
+  test 'should destroy' do
+    delete bulletin_path(@bulletin)
+    assert_not Bulletin.find_by(id: @bulletin.id)
+  end
 end
