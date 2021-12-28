cb08f110baba9b701dd78eb9d67dd5eb48be988e
Dmitriy <reg.dobr@live.ru>
2021-12-28 16:24:20 +0300

    fix: codestyle

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 # Examples:
 #
-  # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-  # Character.create(name: 'Luke', movie: movies.first)
+# movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# Character.create(name: 'Luke', movie: movies.first)
 %i[Hobby Transport Electronics Sports Health].each do |item|
   Category.create(name: item)
 end
