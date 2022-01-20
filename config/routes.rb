ede87808f204487f20bddfeb124363e557433728
Dmitriy <reg.dobr@live.ru>
2022-01-20 16:59:56 +0300

    auth route name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  post '/auth/:provider', to: 'auth#request', as: :sessions
+  post '/auth/:provider', to: 'auth#request', as: :auth_request
   get '/auth/:provider/callback', to: 'web/auth#callback', as: :callback_auth
 
   scope module: :web do
