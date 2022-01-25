2c11f73b98bb9136c7b6792c6ce5c6b22445e0ef
dmitriy <d.dobrenkiy@hotmail.com>
2022-01-25 23:22:45 +0300

    feat: profile tests

--- test/controllers/profile_controller_test.rb
+++ test/controllers/profile_controller_test.rb
@@ -3,7 +3,15 @@
 require 'test_helper'
 
 class ProfileControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get index' do
+    user = users(:one)
+    sign_in user
+    get profile_path
+    assert_response :success
+  end
+
+  test 'should not get index if unauthorised' do
+    get profile_path
+    assert_redirected_to root_path
+  end
 end
