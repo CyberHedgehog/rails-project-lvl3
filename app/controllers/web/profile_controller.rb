1f0e09ad962ee680d7852d392bb3a97de1128ce7
dmitriy <d.dobrenkiy@hotmail.com>
2022-02-09 15:01:32 +0300

    refactor(profile): bulletins query

--- app/controllers/web/profile_controller.rb
+++ app/controllers/web/profile_controller.rb
@@ -3,6 +3,7 @@
 class Web::ProfileController < Web::ApplicationController
   before_action :authenticate_user!
   def index
-    @bulletins = Bulletin.order(created_at: :desc).where(user_id: current_user.id).page params[:page]
+    # @bulletins = Bulletin.order(created_at: :desc).where(user_id: current_user.id).page params[:page]
+    @bulletins = current_user.bulletins.order(created_at: :desc).page(params[:page])
   end
 end
